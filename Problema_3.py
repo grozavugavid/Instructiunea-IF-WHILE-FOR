3430da32f1d21c97ee8cd654b46ecbb76db3c42d
grozavudavid <70814477+grozavugavid@users.noreply.github.com>
2020-11-16 13:38:34 +0200

    Add files via upload

---
+++ Problema_3.py
@@ -0,0 +1,10 @@
+import math
+m=int(input("Introduceti un numar m:"))
+n=int(input("Introduceti un numar n:"))
+i=int(input("Introduceti un numar i:"))
+i=log(n,m)
+e=int(i)
+if i-e==0:
+    print("n este puterea lui m")
+else:
+    print("n nu este puterea lui m")
---
+++ Problema_4.py
@@ -0,0 +1,11 @@
+m=int(input("Introduceti numaratorul fractiei nr.1:"))
+n=int(input("Introduceti numitorul fractiei nr.1:"))
+l=int(input("Introduceti numaratorul fractiei nr.2"))
+y=int(input("Introduceti numitorul fractiei nr.2"))
+s1=m*x+n*l
+s2=n*l
+print("s1",/,"s2")
+if s1>s2:
+    print("i=s2")
+else:
+    print("i=s1")
